29026098add02111b0ea8536a12ae40a52413e8e
Isaac Kralik <104790651+IsaacKralik@users.noreply.github.com>
2023-05-05 12:31:14 -0300

    Add files via upload

---
+++ Retang/src/ret.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author UNIP
+ */
+public class ret {
+    int base,altura,area;
+    
+}
+
+public class void calcularArea(int pbase, int paltura) {
+    this.area = pbase * paltura;
+    System.out.println("A área do triangulo é: " +this.area)
+ 
+}
+
+public class UsaClasses {
+    
+    public static void main(String[] args) {
+        ret obj_ret = New Retangulo ();
+        obj_ret.base = 10;
+        obj_ret.altura = 15
+                
+                
+    }
+    
+   
+}
