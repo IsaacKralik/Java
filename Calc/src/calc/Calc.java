687868f8ee6fc4bf6dedbc5af97ca6f3d78ef0c4
Isaac Kralik <104790651+IsaacKralik@users.noreply.github.com>
2023-05-05 12:37:00 -0300

    Calc_Jframe

---
+++ Calc/src/calc/Calc.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+import java.util.Scanner; // biblioteca para inserir valor
+
+/**
+ *
+ * @author UNIP
+ */
+//public class Calc {
+
+    /**
+     * @param args the command line arguments
+     */
+  //  public static void main(String[] args) {
+    //    // TODO code application logic here
+   //   Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
+    //  double n1,n2;
+      
+    //  System.out.printf("Informe o primeiro valor: ");
+  //n1 = ler.nextdouble(); // 3. entrada de dados (lendo um valor inteiro)
+
+     
+        
+        
+        
+        
+    }    
+}
